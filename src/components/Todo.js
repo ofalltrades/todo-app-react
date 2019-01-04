ffd94aeb7db7d48fd999553dfe55de26383538be
Jake Johnson <kognizant.kog@gmail.com>
2019-01-03 21:42:06 -0500

    make remove button a styled component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const RemoveButton = styled.button`
+  display: inline-block;
+  border-radius: 9px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 2rem;
+  background: red;
+  color: white;
+  border: 2px solid white;
+`
 
 const Todo = ({ text, complete, onClickText, onClickRemove }) => (
   <li>
@@ -10,7 +22,7 @@ const Todo = ({ text, complete, onClickText, onClickRemove }) => (
       {text}
     </span>
 
-    <span onClick={onClickRemove}> -</span>
+    <RemoveButton onClick={onClickRemove}>-</RemoveButton>
   </li>
 )
 
